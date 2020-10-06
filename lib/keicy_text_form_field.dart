17f6c5618ad8c9e469b66dfff5599412c22d496f
FlutterDev <Keicy6778@gmail.com>
2020-10-06 11:26:43 +0100

    2020-10-06
    Signed-off-by: FlutterDev <Keicy6778@gmail.com>

--- lib/keicy_text_form_field.dart
+++ lib/keicy_text_form_field.dart
@@ -7,9 +7,11 @@ import 'package:provider/provider.dart';
 class KeicyTextFormField extends StatelessWidget {
   KeicyTextFormField({
     Key key,
-    this.initialValue,
     @required this.width,
     @required this.height,
+    @required this.widthDp,
+    this.initialValue,
+    this.controller,
     this.fixedHeightState = true,
     this.prefixIcons = const [],
     this.suffixIcons = const [],
@@ -18,29 +20,24 @@ class KeicyTextFormField extends StatelessWidget {
     this.enableShowPassword = false,
     this.iconSpacing = 10,
     this.label = "",
-    this.labelFontSize,
-    this.labelColor,
+    this.labelStyle,
     this.labelSpacing = 10,
     this.border = const Border(bottom: BorderSide(width: 1, color: Colors.black)),
     this.errorBorder = const Border(bottom: BorderSide(width: 1, color: Colors.red)),
     this.borderRadius = 0,
-    this.textFontSize = 20,
-    this.textColor = Colors.black,
+    this.textStyle = const TextStyle(fontSize: 20, color: Colors.black),
+    this.hintStyle = const TextStyle(fontSize: 20, color: Colors.grey),
     this.hintText = "",
-    this.hintTextFontSize,
-    this.hintTextColor = Colors.grey,
-    this.fontFamily,
     this.contentHorizontalPadding = 5,
     this.contentVerticalPadding = 5,
     this.textAlign = TextAlign.left,
     this.keyboardType = TextInputType.text,
     this.validatorHandler,
     this.onSaveHandler,
-    this.fillColor = Colors.white,
+    this.fillColor = Colors.transparent,
     this.autovalidate = false,
     this.obscureText = false,
     this.autofocus = false,
-    this.controller,
     this.onChangeHandler,
     this.onTapHandler,
     this.onFieldSubmittedHandler,
@@ -52,11 +49,13 @@ class KeicyTextFormField extends StatelessWidget {
     this.inputFormatters,
     this.focusNode,
   }) : super(key: key);
-  final String initialValue;
+
   final double width;
   final double height;
+  final double widthDp;
+  final String initialValue;
+  final TextEditingController controller;
   final bool fixedHeightState;
-
   final List<Widget> prefixIcons;
   final List<Widget> suffixIcons;
   final bool isPrefixIconOutofField;
@@ -64,8 +63,7 @@ class KeicyTextFormField extends StatelessWidget {
   final bool enableShowPassword;
   final double iconSpacing;
   final String label;
-  final double labelFontSize;
-  final Color labelColor;
+  final TextStyle labelStyle;
   final double labelSpacing;
   final Border border;
   final Border errorBorder;
@@ -73,13 +71,10 @@ class KeicyTextFormField extends StatelessWidget {
   final double contentHorizontalPadding;
   final double contentVerticalPadding;
 
-  final double textFontSize;
-  final Color textColor;
+  final TextStyle textStyle;
+  final TextStyle hintStyle;
   final String hintText;
-  final Color hintTextColor;
-  double hintTextFontSize;
   final Color fillColor;
-  final String fontFamily;
 
   double errorStringFontSize;
   final focusNode;
@@ -90,12 +85,11 @@ class KeicyTextFormField extends StatelessWidget {
   final bool autovalidate;
   final bool obscureText;
   final Function validatorHandler;
-  final Function onSaveHandler;
-  final TextEditingController controller;
-  final Function onChangeHandler;
+  final Function(String) onSaveHandler;
+  final Function(String) onChangeHandler;
+  final Function(String) onFieldSubmittedHandler;
   final Function onTapHandler;
   final Function onEditingCompleteHandler;
-  final Function onFieldSubmittedHandler;
   final bool autofocus;
   final int maxLines;
   final TextInputAction textInputAction;
@@ -104,8 +98,7 @@ class KeicyTextFormField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    BorderSide hideBorderSide = BorderSide(width: 0, color: fillColor);
-    if (errorStringFontSize == null) errorStringFontSize = textFontSize * 0.8;
+    if (errorStringFontSize == null) errorStringFontSize = textStyle.fontSize * 0.8;
 
     return Material(
       color: Colors.transparent,
@@ -130,9 +123,9 @@ class KeicyTextFormField extends StatelessWidget {
             }
 
             if (enableShowPassword && !customTextFormFieldProvider.isShownPassword) {
-              suffixIcon = FaIcon(FontAwesomeIcons.eye, size: textFontSize * 1.2, color: textColor);
+              suffixIcon = FaIcon(FontAwesomeIcons.eye, size: textStyle.fontSize * 1.2, color: textStyle.color);
             } else if (enableShowPassword && customTextFormFieldProvider.isShownPassword) {
-              suffixIcon = FaIcon(FontAwesomeIcons.eyeSlash, size: textFontSize * 1.2, color: textColor);
+              suffixIcon = FaIcon(FontAwesomeIcons.eyeSlash, size: textStyle.fontSize * 1.2, color: textStyle.color);
             }
 
             return Container(
@@ -141,41 +134,60 @@ class KeicyTextFormField extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  (isPrefixIconOutofField) ? prefixIcon : SizedBox(),
-                  (isPrefixIconOutofField && prefixIcons.length != 0) ? SizedBox(width: iconSpacing) : SizedBox(),
+                  /// prefix Icon
+                  (isPrefixIconOutofField && prefixIcons.length != 0)
+                      ? Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            prefixIcon,
+                            SizedBox(width: widthDp * iconSpacing),
+                          ],
+                        )
+                      : SizedBox(),
                   Expanded(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
+                        /// label
                         (label != "")
-                            ? Text(label, style: TextStyle(fontSize: labelFontSize ?? textFontSize, color: labelColor ?? textColor))
+                            ? Column(
+                                children: [
+                                  Text(label, style: labelStyle),
+                                  SizedBox(height: widthDp * labelSpacing),
+                                ],
+                              )
                             : SizedBox(),
-                        (label != "") ? SizedBox(height: labelSpacing) : SizedBox(),
                         Container(
                           width: double.maxFinite,
                           height: height,
-                          padding: EdgeInsets.symmetric(horizontal: contentHorizontalPadding, vertical: contentVerticalPadding),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: widthDp * contentHorizontalPadding,
+                            vertical: widthDp * contentVerticalPadding,
+                          ),
                           alignment: (keyboardType == TextInputType.multiline) ? Alignment.topLeft : Alignment.center,
                           decoration: BoxDecoration(
                             color: fillColor,
                             border: (customTextFormFieldProvider.errorText == "") ? border : errorBorder,
                             borderRadius: ((customTextFormFieldProvider.errorText == "" && border.isUniform) ||
                                     (customTextFormFieldProvider.errorText != "" && errorBorder.isUniform))
-                                ? BorderRadius.circular(borderRadius)
+                                ? BorderRadius.circular(widthDp * borderRadius)
                                 : null,
                           ),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             crossAxisAlignment: CrossAxisAlignment.center,
                             children: <Widget>[
+                              /// prefix icon
                               (!isPrefixIconOutofField && prefixIcons.length != 0) ? prefixIcon : SizedBox(),
-                              (!isPrefixIconOutofField && prefixIcons.length != 0) ? SizedBox(width: iconSpacing) : SizedBox(),
+                              (!isPrefixIconOutofField && prefixIcons.length != 0) ? SizedBox(width: widthDp * iconSpacing) : SizedBox(),
                               Expanded(
                                 child: TextFormField(
                                   focusNode: focusNode,
-                                  initialValue: initialValue,
-                                  style: TextStyle(color: textColor, fontSize: textFontSize, fontFamily: fontFamily),
+                                  initialValue: controller == null ? initialValue : null,
+                                  controller: controller,
+                                  style: textStyle,
                                   autofocus: autofocus,
                                   maxLines: maxLines,
                                   textInputAction: textInputAction,
@@ -183,18 +195,18 @@ class KeicyTextFormField extends StatelessWidget {
                                   textAlign: textAlign,
                                   keyboardType: keyboardType,
                                   autovalidate: autovalidate,
-                                  controller: controller,
                                   decoration: InputDecoration(
                                     errorStyle: TextStyle(fontSize: 0, color: fillColor, height: 0),
                                     isDense: true,
+                                    isCollapsed: true,
                                     hintText: hintText,
-                                    hintStyle: TextStyle(fontSize: hintTextFontSize ?? textFontSize, color: hintTextColor, fontFamily: fontFamily),
-                                    border: UnderlineInputBorder(borderSide: hideBorderSide),
-                                    enabledBorder: UnderlineInputBorder(borderSide: hideBorderSide),
-                                    focusedBorder: UnderlineInputBorder(borderSide: hideBorderSide),
-                                    focusedErrorBorder: UnderlineInputBorder(borderSide: hideBorderSide),
-                                    errorBorder: UnderlineInputBorder(borderSide: hideBorderSide),
-                                    disabledBorder: UnderlineInputBorder(borderSide: hideBorderSide),
+                                    hintStyle: hintStyle,
+                                    border: InputBorder.none,
+                                    enabledBorder: InputBorder.none,
+                                    focusedBorder: InputBorder.none,
+                                    focusedErrorBorder: InputBorder.none,
+                                    errorBorder: InputBorder.none,
+                                    disabledBorder: InputBorder.none,
                                     filled: true,
                                     fillColor: fillColor,
                                     contentPadding: EdgeInsets.zero,
@@ -224,7 +236,7 @@ class KeicyTextFormField extends StatelessWidget {
                                 ),
                               ),
                               ((!isSuffixIconOutofField && suffixIcons.length != 0) || enableShowPassword)
-                                  ? SizedBox(width: iconSpacing)
+                                  ? SizedBox(width: widthDp * iconSpacing)
                                   : SizedBox(),
                               GestureDetector(
                                 child: ((!isSuffixIconOutofField && suffixIcons.length != 0) || enableShowPassword) ? suffixIcon : SizedBox(),
@@ -239,14 +251,14 @@ class KeicyTextFormField extends StatelessWidget {
                         ),
                         (customTextFormFieldProvider.errorText != "")
                             ? Container(
-                                height: textFontSize,
+                                height: errorStringFontSize + widthDp * 8,
                                 alignment: Alignment.centerLeft,
                                 child: Text(
                                   customTextFormFieldProvider.errorText,
                                   style: TextStyle(fontSize: errorStringFontSize, color: Colors.red),
                                 ),
                               )
-                            : (fixedHeightState) ? SizedBox(height: errorStringFontSize) : SizedBox(),
+                            : (fixedHeightState) ? SizedBox(height: errorStringFontSize + widthDp * 8) : SizedBox(),
                       ],
                     ),
                   ),
